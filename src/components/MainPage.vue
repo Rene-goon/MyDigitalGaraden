6bb157fc0386f16a41f430d5a8b8cc4c06fdc3d5
Rene A Carbajal <Rac308@txstate.edu>
2023-12-23 00:08:43 -0600

    init main page

---
+++ src/components/MainPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="main-page">
+      <table>
+        <thead>
+          <tr>
+            <th>Contact me:</th>
+            <th>Employee email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Rene A Carbajal</td>
+            <td>renecarbajal2017@gmail.com</td>
+          </tr>          
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      name: 'main-page',
+    }
+  </script>
+  
+  <style scoped></style>
